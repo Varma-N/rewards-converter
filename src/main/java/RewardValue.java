714b0fcbc297950bd06280e000772e9784dd5bad
NANDI MADAN GOPAL VARMA <149928844+Varma-N@users.noreply.github.com>
2024-09-10 14:48:23 +0530

    final solution

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,38 +2,34 @@ package jpmorgan_virtual_JS;
 
 public class RewardValue {
 	private double cashValue;
-    private double milesValue;
+    private int milesValue;
     
     public RewardValue(double cashValue) {
     	this.cashValue = cashValue;
     	this.milesValue = convertCashToMiles(cashValue);
     }
     
-    public RewardValue(double milesValue, boolean isMiles) {
-    	if (isMiles) {
+    public RewardValue(int milesValue) {
             this.milesValue = milesValue;
             this.cashValue = convertMilesToCash(milesValue);
-        } else {
-            throw new IllegalArgumentException("The boolean flag must be true for miles value constructor.");
-        }
     }
 	
     public double getCashValue() {
     	return cashValue;
     }
     
-    public double getMilesValue() {
+    public int getMilesValue() {
     	return milesValue;
     }
     
     
-    private double convertCashToMiles(double cashValue){
+    private int convertCashToMiles(double cashValue){
     	 final double MILES_PER_DOLLAR = 285.71; // 1 dollar = 285.71 miles
-         return cashValue * MILES_PER_DOLLAR;
+         return (int) Math.round(cashValue * MILES_PER_DOLLAR);
     }
     
     private double convertMilesToCash(double milesValue) {
-        final double CASH_PER_MILE = 0.0035;
+        final double CASH_PER_MILE = 0.0035; // 1 mile = 0.0035 dollars
         return milesValue * CASH_PER_MILE;
     }
 }
